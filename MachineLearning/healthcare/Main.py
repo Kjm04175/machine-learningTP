5447d2e8588fafe696988bd3f6671d9ec23c0589
sdj3261 <63071967+sdj3261@users.noreply.github.com>
2020-11-15 18:35:15 +0900

    Update Main.py
    + Evaluation

--- MachineLearning/healthcare/Main.py
+++ MachineLearning/healthcare/Main.py
@@ -3,6 +3,9 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import warnings
+from yellowbrick.classifier import ROCAUC
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix
 from sklearn.preprocessing import MinMaxScaler, RobustScaler, StandardScaler
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
@@ -136,7 +139,7 @@ train_data= columnEncoding(train_data.columns, train_data)
 X = train_data.iloc[:, :-1]
 X=ScalingData(X, m_scaler)
 Y = train_data.iloc[:, -1]
-X, _, Y, _ = train_test_split(X, Y, test_size=0.8, random_state=42)
+X, X_test, Y, Y_test = train_test_split(X, Y, test_size=0.8, random_state=42)
 
 select_k_best(X, Y, len(X.columns), visualization=True)
 #nan이 존재하는 column만 따로 분류
@@ -154,23 +157,21 @@ from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import GridSearchCV
 param_rf = {
-    'n_estimators':np.arange(50, 210, 40),
+    'n_estimators':np.arange(30, 150, 50),
     'criterion':['gini','entropy'],
-    'max_depth':[None,1,3,5,10, 15, 20],#
+    'max_depth':[1,5,10,20],#
     'bootstrap':[True, False]
 }
 param_bagging={
-    'n_estimators':np.arange(4,20,2),
+    'n_estimators':np.arange(4,20,4),
     'bootstrap':[True,False],
 }
 param_svc={
-    'C':[0.5, 1.0, 2.0, 3.0, 4.0, 5.0],
+    'C':[0.05, 0.1, 0.5, 1.0, 3.0, 5.0],
     'kernel':['linear', 'poly', 'rbf', 'sigmoid', 'rbf'],
-    'degree':[3, 4, 5, 6],
     'gamma':['scale', 'auto']
 }
 param_knn={
-    'weight':['uniform','distance'],
     'algorithm':['auto', 'ball_tree', 'kd_tree', 'brute'],
     'leaf_size':[5, 10, 30, 50, 100],
     'p':[1,2,3,4,5]
@@ -185,12 +186,78 @@ def process(X, Y, model, param, cv=10):
     grid_search =GridSearchCV(model, param_grid=param, cv=cv)
     grid_search.fit(X, Y)
     return grid_search
+"""
 print('process start')
 rf_result=process(X, Y, rf, param_rf)
 rf_result = pd.DataFrame(rf_result.cv_results_)
 rf_result.to_csv('rf_preprocessed.csv')
-print('process done')
-#프로세스가 굉장히 오래걸린다. 이를 해결할 수 있는 방법은?
+print('rf process done')
+
+bag_result=process(X, Y, bagging, param_bagging)
+bag_result = pd.DataFrame(bag_result.cv_results_)
+bag_result.to_csv('bagging_preprocessed.csv')
+print('bagging process done')
+
+svc_result=process(X, Y, svc, param_svc)
+svc_result = pd.DataFrame(svc_result.cv_results_)
+svc_result.to_csv('svc_preprocessed.csv')
+print('svc process done')
+**
+knn_result=process(X, Y, knn, param_knn)
+knn_result = pd.DataFrame(knn_result.cv_results_)
+knn_result.to_csv('knn_preprocessed.csv')
+print('knn process done')
+"""
 ###################################################################
 # Evaluation
 ###################################################################
+#Random_Forest Evaluation
+rf_best = RandomForestClassifier(criterion="entropy", bootstrap=False,max_depth=10)
+rf_best.fit(X,Y)
+print(confusion_matrix(Y_test,rf_best.predict(X_test)))
+plt.figure(figsize=(2, 2))
+sns.heatmap(metrics.confusion_matrix(Y_test,rf_best.predict(X_test)), annot=True, fmt='.2f', linewidths=.1, cmap='Blues')
+
+visualizer = ROCAUC(rf_best, classes=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], micro=False, macro=True, per_class=False)
+visualizer.fit(X, Y)
+visualizer.score(X, Y)
+visualizer.show()
+
+#Bagging Classifier Evaluation
+bagging_best = BaggingClassifier(bootstrap=True, n_estimators=16)
+bagging_best.fit(X,Y)
+print(confusion_matrix(Y_test,bagging_best.predict(X_test)))
+plt.figure(figsize=(2, 2))
+sns.heatmap(metrics.confusion_matrix(Y_test,bagging_best.predict(X_test)), annot=True, fmt='.2f', linewidths=.1, cmap='Blues')
+
+
+visualizer = ROCAUC(bagging_best, classes=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], micro=False, macro=True, per_class=False)
+visualizer.fit(X, Y)
+visualizer.score(X, Y)
+visualizer.show()
+
+#Support Vector Machine Evaluation
+svc_best = SVC(C= 5.0, gamma = 'scale', kernel = 'linear')
+svc_best.fit(X,Y)
+print(confusion_matrix(Y_test,svc_best.predict(X_test)))
+plt.figure(figsize=(2, 2))
+sns.heatmap(metrics.confusion_matrix(Y_test,svc_best.predict(X_test)), annot=True, fmt='.2f', linewidths=.1, cmap='Blues')
+
+
+visualizer = ROCAUC(svc_best, classes=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], micro=False, macro=True, per_class=False)
+visualizer.fit(X, Y)
+visualizer.score(X, Y)
+visualizer.show()
+
+#KNN Evaluation
+knn_best = KNeighborsClassifier(algorithm = "auto", leaf_size =10,p=1)
+knn_best.fit(X,Y)
+print(confusion_matrix(Y_test,knn_best.predict(X_test)))
+plt.figure(figsize=(2, 2))
+sns.heatmap(metrics.confusion_matrix(Y_test,knn_best.predict(X_test)), annot=True, fmt='.2f', linewidths=.1, cmap='Blues')
+
+
+visualizer = ROCAUC(knn_best, classes=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], micro=False, macro=True, per_class=False)
+visualizer.fit(X, Y)
+visualizer.score(X, Y)
+visualizer.show()
